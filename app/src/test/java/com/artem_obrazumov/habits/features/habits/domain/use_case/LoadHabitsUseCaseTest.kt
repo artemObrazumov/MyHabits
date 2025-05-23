82d80ab2d2c15cd41d0087f876d8afdbe636e700
artemObrazumov <a.obraz60022006@gmail.com>
2025-05-23 16:23:07 +0300

    tested LoadHabitsUseCaseTest and HabitsListScreenViewModel

--- app/src/test/java/com/artem_obrazumov/habits/features/habits/domain/use_case/LoadHabitsUseCaseTest.kt
+++ app/src/test/java/com/artem_obrazumov/habits/features/habits/domain/use_case/LoadHabitsUseCaseTest.kt
@@ -1,9 +1,20 @@
 package com.artem_obrazumov.habits.features.habits.domain.use_case
 
+import com.artem_obrazumov.habits.common.domain.Result
+import com.artem_obrazumov.habits.common.domain.UnknownError
 import com.artem_obrazumov.habits.features.habits.domain.data_source.HabitsLocalDataSource
+import com.artem_obrazumov.habits.features.habits.domain.model.GoalType
+import com.artem_obrazumov.habits.features.habits.domain.model.Habit
+import com.artem_obrazumov.habits.features.habits.domain.model.ProgressFrequency
 import org.junit.Test
 import io.mockk.coEvery
 import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 class LoadHabitsUseCaseTest {
 
@@ -11,5 +22,36 @@ class LoadHabitsUseCaseTest {
     private val loadHabitUseCase = LoadHabitsUseCase(mockLocalDataSource)
 
     @Test
+    fun `returns success result correctly`() = runTest {
+        val habits = listOf(
+            Habit(
+                id = 1,
+                name = "Test habit",
+                measurement = "meters",
+                goalType = GoalType.SINGLE,
+                frequency = ProgressFrequency.DAILY,
+                progress = 0.1,
+                goal = 10.0,
+                startedAt = LocalDate.parse("2025-05-01"),
+                editedAt = LocalDateTime.parse("2025-05-01T12:00:00"),
+                usersCount = 1
+            )
+        )
+        coEvery { mockLocalDataSource.observeHabitsFromDatabase() } returns flowOf(habits)
 
+        val result = loadHabitUseCase()
+        assert(result is Result.Success)
+
+        assertEquals((result as Result.Success).data.first(), habits)
+    }
+
+    @Test
+    fun `returns failure result correctly`() = runTest {
+        coEvery { mockLocalDataSource.observeHabitsFromDatabase() } throws Exception("Test exception")
+
+        val result = loadHabitUseCase()
+        assert(result is Result.Failure)
+
+        assertEquals((result as Result.Failure).error, UnknownError)
+    }
 }
